578a45a5a9d8dbdfcef0d2769431ccfa0950f83a
Jaspal Singh <jaspalsinghpersonal9@gmail.com>
2021-08-26 21:46:50 +0530

    Made tabs working on mobile using javascript

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
 const options = document.querySelectorAll(".option");
+const images = document.querySelectorAll(".planet_img");
+const descriptions = document.querySelectorAll(".description");
 
-options.forEach((option) => {
-  option.addEventListener("click", handleOptionToggle, false);
+options.forEach((option, i) => {
+  option.addEventListener("click", () => handleOptionToggle(event, i), false);
 });
 
-function handleOptionToggle(event) {
+function handleOptionToggle(event, i) {
   document.querySelector(".option-active").classList.remove("option-active");
   event.target.classList.add("option-active");
-  // document.querySelector(".img-show").classList.remove("img-show");
+  handlePlanetImages(i);
+  handlePlanetDescription(i);
+}
+
+function handlePlanetImages(i) {
+  images.forEach((img) => {
+    if (img.classList.contains("img-show")) {
+      img.classList.remove("img-show");
+    }
+  });
+
+  images[i].classList.add("img-show");
+  if (i === 2) {
+    document.querySelector(".surface-geology").classList.add("img-show");
+  } else {
+    document.querySelector(".surface-geology").classList.remove("img-show");
+  }
+}
+
+function handlePlanetDescription(i) {
+  descriptions.forEach((description) => {
+    if (description.classList.contains("show-description")) {
+      description.classList.remove("show-description");
+    }
+  });
+  descriptions[i].classList.add("show-description");
 }
