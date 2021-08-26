ff0fa7bb74a5deaca7c02292ef91d9ed08fdec92
Jaspal Singh <jaspalsinghpersonal9@gmail.com>
2021-08-26 14:21:44 +0530

    Added JavaScript for option toggle in javascript

---
+++ app.js
@@ -0,0 +1,10 @@
+const options = document.querySelectorAll(".option");
+
+options.forEach((option) => {
+  option.addEventListener("click", handleOptionToggle, false);
+});
+
+function handleOptionToggle(event) {
+  document.querySelector(".option-active").classList.remove("option-active");
+  event.target.classList.add("option-active");
+}
